b2ce156d6694709c918d290c34d5cad0f08c0c3c
mirzamanib <mirzamanib@gmail.com>
2020-01-24 18:12:25 +0330

    page layout, table and pagination added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.page(params[:page] || 1).per(params[:per] || 10)
     render :index
   end
 end
