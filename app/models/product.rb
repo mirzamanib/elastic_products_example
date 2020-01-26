4c500a4212583497b5f76fdc1d3dc7b7741abb95
mirzamanib <mirzamanib@gmail.com>
2020-01-26 21:29:40 +0330

    refactor to searchable module

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,48 +1,11 @@
 class Product < ApplicationRecord
   include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  include Searchable
 
-  index_name Rails.application.class.module_parent_name.underscore
 
   validates_presence_of(:title, :price)
   validates_uniqueness_of :title
 
-  settings index: { number_of_shards: 1 } do
-    mapping dynamic: false do
-      indexes :title, analyzer: 'english'
-      indexes :description, analyzer: 'english'
-      indexes :tags, analyzer: 'english'
-    end
-  end
-
-  def as_indexed_json(options={})
-    self.as_json(only: [:id, :title, :description, :tags])
-  end
-
-
-  after_commit on: [:create] do
-    __elasticsearch__.index_document
-  end
-
-  after_commit on: [:update] do
-      __elasticsearch__.update_document
-  end
-
-  after_commit on: [:destroy] do
-    __elasticsearch__.delete_document
-  end
-
-  def self.search(query)
-    __elasticsearch__.search(
-      query: {
-        query_string: {
-          type: 'phrase',
-          query: "*#{query}*",
-          fields: ['title^2', 'description', 'tags']
-        }
-      }
-    )
-  end
 end
 
 # Delete the previous articles index in Elasticsearch
